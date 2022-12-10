45df6a4f6fe849932d6667b5a47ef5d882d1100a
david <david@plisky.net>
2022-12-10 16:04:29 +0000

    inital commit

---
+++ src/test/java/com/passwordmanager/frontend/PasswordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passwordmanager.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
